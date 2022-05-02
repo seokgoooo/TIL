c695c6f94f926b8ca1a8056078ac043088561f94
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-02 15:28:14 +0900

    Add files via upload

---
+++ TRN01/JS/main.js
@@ -0,0 +1,13 @@
+// document.querySelector('.header').style.display = 'none';
+// $('.header').hide().show();
+// 같은 말이다
+
+$(function () {
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        vertical: true,
+        dots: true,
+    })
+})
