4d989fae1309599e58e33129f36b68dccbf22274
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-09 15:02:35 +0900

    Add files via upload

--- TRN02/JS/main.js
+++ TRN02/JS/main.js
@@ -1,9 +1,21 @@
 $(function () {
     ///////////////////////////////////////////
-
     $('.top_banner i').on('click', function () {
-        $('.top_banner').slideUp(1000);
+        $('.top_banner').slideUp();
     })
 
+    $('.main_slider').on('init reInit afterChange', function () {
+        let current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    })
+
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    })
     ///////////////////////////////////////////
 })
