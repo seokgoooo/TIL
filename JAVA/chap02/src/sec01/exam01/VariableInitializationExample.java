6f5cc6776873916d9a3926eaddc07cf8ed1f412e
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-10 00:11:07 +0900

    Add files via upload

---
+++ JAVA/chap02/src/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,11 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		int value;
+//		int result = value + 10;
+		System.out.println(result);
+	}
+
+}
