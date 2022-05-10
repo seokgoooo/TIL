83844985322fb730136dcc8aaf1516301ee9e62f
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-10 12:40:32 +0900

    Add files via upload

--- TRN02/JS/main.js
+++ TRN02/JS/main.js
@@ -17,5 +17,31 @@ $(function () {
         pauseOnHover: false,
         pauseOnFocus: false,
     })
+
+    $('.movie01 .dec i:nth-of-type(1)').on('click', function () {
+        $('.movie01 video').trigger('play');
+    })
+
+    $('.movie01 .dec i:nth-of-type(2)').on('click', function () {
+        $('.movie01 video').trigger('pause');
+    })
+
+    $('.player').YTPlayer({
+        videoURL: 'https://youtu.be/z9Ug-3qhrwY',
+        containment: '.movie02',
+        autoPlay: true,
+        mute: true,
+        startAt: 0,
+        opacity: 1,
+        showControls: false,
+    });
+
+    $('.movie02 i:nth-of-type(1)').on('click', function () {
+        $('#bgndVideo').YTPPlay();
+    })
+
+    $('.movie02 i:nth-of-type(2)').on('click', function () {
+        $('#bgndVideo').YTPPause();
+    })
     ///////////////////////////////////////////
 })
