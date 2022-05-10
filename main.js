c57de1b73deb5ca850ee50cc87c25c82ae2cc5df
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-10 15:35:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+$(function () {
+    $('.main_slider').slick({
+        arrows: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+})
+
 const toggleBtn = document.querySelector('.navbar_toggleBtn');
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icons');
@@ -6,3 +37,4 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
+
