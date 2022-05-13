62d50bcb41cbe54bf2d6c4fe056c660956ed631d
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-13 23:07:08 +0900

    Add files via upload

--- JAVA/chap02/src/sec01/exam01/VariableInitializationExample.java
+++ JAVA/chap02/src/sec01/exam01/VariableInitializationExample.java
@@ -3,8 +3,8 @@ package sec01.exam01;
 public class VariableInitializationExample {
 
 	public static void main(String[] args) {
-		int value;
-//		int result = value + 10;
+		int value = 0;
+		int result = value + 10;
 		System.out.println(result);
 	}
 
