c0ecf7be41c5e8250f3c5e8da8d3b8b3320929a8
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-23 22:51:59 +0900

    ~Q6

--- Green/0523/Question5.java
+++ Green/0523/Question5.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Question5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int result;
+		int[] num = new int[5];
+		int max = 0;
+		
 		for (int i = 0; i < 5; i++) {
-			System.out.println("정수 입력.");
-			int count = sc.nextInt();
-			
+			System.out.print("5개의 정수 입력: ");
+			num[i] = sc.nextInt();
+			if (num[max] < num[i]) {
+				max = i;
+			}
 		}
-		System.out.println("가장 큰 수: " + result);
+		System.out.println("가장 큰 수: " + num[max]);
 	}
 }
--- Green/0523/Question6.java
+++ Green/0523/Question6.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Question6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int result;
-		for (int i = 0; i < 5; i++) {
-			System.out.println("정수 입력.");
-			int count = sc.nextInt();
-			
+		int result = 0;
+		
+		System.out.print("영어 입력: ");
+		String latin = sc.nextLine();			
+		
+		for (int i = 0; i < latin.length(); i++) {
+			if (latin.charAt(i) == 'a' || latin.charAt(i) == 'e' || latin.charAt(i) == 'i' || latin.charAt(i) == 'o' || latin.charAt(i) == 'u') {
+				result++;
+			}
 		}
-		System.out.println("가장 큰 수: " + result);
+		System.out.println("모음의 수: " + result);
 	}
 }
