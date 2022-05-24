a0a15cd012168921b4752f5d49c59ea0d7c9730b
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-24 10:33:13 +0900

    Q3

--- Green/0523/Question3.java
+++ Green/0523/Question3.java
@@ -12,39 +12,39 @@ public class Question3 {
 		int coke = 0;
 		int soda = 0;
 		
-		while (money >= 0) {
+		while (money > 0) {
 			System.out.println("*** 1번 콜라 1500원 *** 2번 사이다 1300원 *** 3번 계산 ***");
 			System.out.println("1, 2, 3 중에 고르세요");
 			int choose = sc.nextInt();
 				if (choose == 1) {
-					if (money > 1500) {
-					money -= 1500;
-					coke++;
-					System.out.println("콜라: " + coke + "캔");
-					System.out.println("사이다: " + soda + "캔");
-					System.out.println("남은 돈: " + money);
+					if (money >= 1500) {
+						money -= 1500;
+						coke++;
+						System.out.println("콜라: " + coke + "캔");
+						System.out.println("사이다: " + soda + "캔");
+						System.out.println("남은 돈: " + money);
 					} else {
-					System.out.println("남은 돈: " + money);
-					System.out.println("잔액이 부족합니다.");
-					money = -1;
+						System.out.println("남은 돈: " + money);
+						System.out.println("잔액이 부족합니다.");
+						money = -1;
 					}
 				} else if (choose == 2) {
-					if (money > 1300) {
-					money -= 1300;
-					soda++;
-					System.out.println("콜라: " + coke + "캔");
-					System.out.println("사이다: " + soda + "캔");
-					System.out.println("남은 돈: " + money);
+					if (money >= 1300) {
+						money -= 1300;
+						soda++;
+						System.out.println("콜라: " + coke + "캔");
+						System.out.println("사이다: " + soda + "캔");
+						System.out.println("남은 돈: " + money);
 					} else {
-					System.out.println("남은 돈: " + money);
-					System.out.println("잔액이 부족합니다.");
-					money = -1;						
+						System.out.println("남은 돈: " + money);
+						System.out.println("잔액이 부족합니다.");
+						money = -1;						
 					}
 				} else if (choose == 3) {
-					System.out.println("콜라: " + coke + "캔");
-					System.out.println("사이다: " + soda + "캔");
-					System.out.println("거스름돈: " + money);
-					money = -1;
+						System.out.println("콜라: " + coke + "캔");
+						System.out.println("사이다: " + soda + "캔");
+						System.out.println("거스름돈: " + money);
+						money = -1;
 				}
 		}
 	}
