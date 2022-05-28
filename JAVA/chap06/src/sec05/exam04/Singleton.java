a9182708559de353b135a3e81f32bb3423977fcb
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-29 00:15:34 +0900

    06-5

---
+++ JAVA/chap06/src/sec05/exam04/Singleton.java
@@ -0,0 +1,11 @@
+package sec05.exam04;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton() {}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}
+}
