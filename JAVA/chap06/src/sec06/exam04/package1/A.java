4e391a7e7a4bce078626a3928410c4ae96340ceb
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-05-29 21:34:09 +0900

    Add files via upload

---
+++ JAVA/chap06/src/sec06/exam04/package1/A.java
@@ -0,0 +1,13 @@
+package sec06.exam04.package1;
+
+public class A {
+	// 필드
+	A a1 = new A(true);
+	A a2 = new A(1);
+	A a3 = new A("문자열");
+	
+	// 생성자
+	public A(boolean b) {}
+	A(int b) {}
+	private A(String s) {}
+}
