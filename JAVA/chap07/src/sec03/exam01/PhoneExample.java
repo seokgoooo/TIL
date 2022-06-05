bd092a9ef7e0c64cc14e0370a2e98d692f4c91e8
seokgoooo <103179265+seokgoooo@users.noreply.github.com>
2022-06-05 21:31:27 +0900

    7-3

---
+++ JAVA/chap07/src/sec03/exam01/PhoneExample.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+
+public class PhoneExample {
+	public static void main(String[] args) {
+		//Phone phone = new Phone();
+		
+		SmartPhone smartPhone = new SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+}
---
+++ JAVA/chap07/src/sec03/exam02/Cat.java
@@ -0,0 +1,12 @@
+package sec03.exam02;
+
+public class Cat extends Animal {
+	public Cat() {
+		this.kind = "Æ÷À¯·ù";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("¾ß¿Ë");
+	}
+}
