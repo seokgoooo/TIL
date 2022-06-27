751ff4b854f54ecc30e7884da92c620dde7e02f3
구구 <coaffc@gmail.com>
2022-06-27 09:31:48 +0900

    Delete JAVA/Academy/220531-03 Class directory

--- JAVA/Academy/220531-03 Class/LogicTest.java
+++
@@ -1,6 +0,0 @@
-public class LogicTest {
-	public static void main(String[] args) {
-		Logic a = new Logic();
-		a.mainLogic();
-	}
-}
--- JAVA/Academy/220531-03 Class/TestOne.java
+++
@@ -1,7 +0,0 @@
-public class TestOne {
-	private String school;
-
-	public TestOne(String school) {
-		this.school = school;
-	}
-}
